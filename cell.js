eeeb81994a19581c0dc72b39a6f3dada40392c54
Hugos29 <y.hugo.s29@gmail.com>
2020-03-24 23:47:34 +0100

    Initial commit

---
+++ cell.js
@@ -0,0 +1,121 @@
+class Cell {
+
+  constructor(i, j) {
+    this.i = i
+    this.j = j
+    this.walls = [true, true, true, true]
+    this.visited = false
+  }
+
+  checkNeighbors() {
+    const neighbors = []
+
+    const top = cells[index(this.i, this.j - 1)]
+    const right = cells[index(this.i + 1, this.j)]
+    const bottom = cells[index(this.i, this.j + 1)]
+    const left = cells[index(this.i - 1, this.j)]
+
+    if (top && !top.visited) {
+      neighbors.push(top)
+    }
+    if (right && !right.visited) {
+      neighbors.push(right)
+    }
+    if (bottom && !bottom.visited) {
+      neighbors.push(bottom)
+    }
+    if (left && !left.visited) {
+      neighbors.push(left)
+    }
+
+    if (neighbors.length > 0) {
+      const r = floor(random(0, neighbors.length));
+      return neighbors[r]
+    } else {
+      return undefined
+    }
+  }
+
+  highlight(c = [0, 0, 255, 100]) {
+    const x = this.i * w
+    const y = this.j * w
+    noStroke()
+    fill(c)
+    rect(x, y, w, w)
+  }
+
+  show() {
+    const x = this.i * w;
+    const y = this.j * w;
+    stroke(255);
+    if (this.walls[0]) {
+      line(x, y, x + w, y);
+    }
+    if (this.walls[1]) {
+      line(x + w, y, x + w, y + w);
+    }
+    if (this.walls[2]) {
+      line(x + w, y + w, x, y + w);
+    }
+    if (this.walls[3]) {
+      line(x, y + w, x, y);
+    }
+
+    if (this.visited) {
+      noStroke();
+      fill(0, 0, 255, 100);
+      rect(x, y, w, w);
+    }
+  }
+
+  print() {
+    const x = this.i * w;
+    const y = this.j * w;
+    stroke(0);
+    if (this.walls[0]) {
+      line(x, y, x + w, y);
+    }
+    if (this.walls[1]) {
+      line(x + w, y, x + w, y + w);
+    }
+    if (this.walls[2]) {
+      line(x + w, y + w, x, y + w);
+    }
+    if (this.walls[3]) {
+      line(x, y + w, x, y);
+    }
+
+    noStroke()
+    fill(255)
+    rect(x, y, w, w)
+
+  }
+
+}
+
+
+function index(i, j) {
+  if (i < 0 || j < 0 || i > width / w - 1 || j > height / w - 1) {
+    return -1;
+  }
+  return i + j * width / w;
+}
+
+function removeWalls(a, b) {
+  let x = a.i - b.i;
+  if (x === 1) {
+    a.walls[3] = false;
+    b.walls[1] = false;
+  } else if (x === -1) {
+    a.walls[1] = false;
+    b.walls[3] = false;
+  }
+  let y = a.j - b.j;
+  if (y === 1) {
+    a.walls[0] = false;
+    b.walls[2] = false;
+  } else if (y === -1) {
+    a.walls[2] = false;
+    b.walls[0] = false;
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,78 @@
+const w = 5
+const cells = []
+let current
+const prev = []
+/**
+ * 1  => generating
+ * 0  => done
+ * -1 => going back
+ **/
+let state = 1
+
+function setup() {
+  createCanvas(800, 800);
+  for (let j = 0; j < width / w; j++) {
+    for (let i = 0; i < height / w; i++) {
+      const cell = new Cell(i, j);
+      cells.push(cell);
+    }
+  }
+  current = cells[0]
+}
+
+function draw() {
+  background(0)
+  pixelDensity(6)
+  for (const cell of cells) {
+    cell.show()
+  }
+  switch (state) {
+    case 1:
+      maze(current)
+      break
+    case -1:
+      back()
+      break
+    case 0:
+    default:
+      break
+  }
+  if (count >= (width / w) * (height / w)) {
+    noLoop()
+    state = 0
+    for (const cell of cells) {
+      cell.print()
+    }
+    print()
+  }
+}
+
+let count = 0
+
+function back() {
+  const lastC = prev[prev.length - 1]
+  lastC.highlight('purple')
+  const n = lastC.checkNeighbors()
+  if (n) {
+    state = 1
+    current = lastC
+    count--
+  } else {
+    prev.pop()
+  }
+}
+
+function maze(c) {
+  c.highlight('green')
+  c.visited = true
+  const newC = c.checkNeighbors()
+  count++
+  if (!newC) {
+    c.highlight('red')
+    state = -1
+  } else {
+    removeWalls(c, newC)
+    current = newC
+    prev.push(c)
+  }
+}
