5b96aa43a93e7baca638d43c33ec105d04406a21
Hugos29 <y.hugo.s29@gmail.com>
2020-04-01 13:54:01 +0200

    Random start/end

--- sketch.js
+++ sketch.js
@@ -1,77 +1,82 @@
-const w = 25
-const cells = []
-let current
-const prev = []
+const w = 30;
+const cells = [];
+let current;
+const prev = [];
+const sx = 25;
+const sy = 25;
 /**
  * 1  => generating
  * 0  => done
  * -1 => going back
  **/
-let state = 1
+let state = 1;
 
 function setup() {
-  createCanvas(1000, 1000);
+  createCanvas(w * sx, w * sy);
   for (let j = 0; j < floor(width / w); j++) {
     for (let i = 0; i < floor(height / w); i++) {
       const cell = new Cell(i, j);
       cells.push(cell);
     }
   }
-  current = cells[0]
+  current = cells[0];
 }
 
 function draw() {
-  background(0)
-  pixelDensity(6)
+  background(0);
+  pixelDensity(6);
+  frameRate(60);
   for (const cell of cells) {
-    cell.show()
+    cell.show();
   }
   switch (state) {
     case 1:
-      maze(current)
-      break
+      maze(current);
+      break;
     case -1:
-      back()
-      break
+      back();
+      break;
     case 0:
     default:
-      break
+      break;
   }
   if (count >= floor(width / w) * floor(height / w)) {
-    noLoop()
-    state = 0
+    noLoop();
+    state = 0;
     for (const cell of cells) {
-      cell.print()
+      cell.print();
     }
+    //random(cells).highlight();
+    //random(cells).highlight();
   }
 }
 
-let count = 0
+let count = 0;
 
 function back() {
-  const lastC = prev[prev.length - 1]
-  lastC.highlight('purple')
-  const n = lastC.checkNeighbors()
+  const lastC = prev[prev.length - 1];
+  lastC.highlight("purple");
+  const n = lastC.checkNeighbors();
   if (n) {
-    state = 1
-    current = lastC
-    count--
+    state = 1;
+    current = lastC;
+    count--;
   } else {
-    prev.pop()
+    prev.pop();
   }
 }
 
 function maze(c) {
-  c.highlight('green')
-  c.visited = true
-  const newC = c.checkNeighbors()
-  count++
+  c.highlight("green");
+  c.visited = true;
+  const newC = c.checkNeighbors();
+  count++;
   if (!newC) {
-    c.highlight('red')
-    state = -1
+    c.highlight("red");
+    state = -1;
   } else {
-    removeWalls(c, newC)
-    current = newC
-    prev.push(c)
+    removeWalls(c, newC);
+    current = newC;
+    prev.push(c);
   }
 }
